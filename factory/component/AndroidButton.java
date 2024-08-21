f552439e65f591d667337fb69a98d4af8f79a376
Jai Prakash Kumar <37208098+Prakashkmr7@users.noreply.github.com>
2024-08-21 16:37:04 +0530

    Create AndroidButton.java

---
+++ factory/component/AndroidButton.java
@@ -0,0 +1,11 @@
+package com.example.LLD.factory.component;
+
+public class AndroidButton implements Button{
+
+    @Override
+    public void printButton() {
+        System.out.println("Android button...");
+
+    }
+
+}
