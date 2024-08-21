6572deb2b5d6e905d9d39c97d611fa7a27bf1a55
Jai Prakash Kumar <37208098+Prakashkmr7@users.noreply.github.com>
2024-08-21 16:38:11 +0530

    Create IosButton.java

---
+++ factory/component/IosButton.java
@@ -0,0 +1,13 @@
+package com.example.LLD.factory.component;
+
+import com.example.LLD.factory.component.Button;
+
+public class IosButton implements Button {
+
+    @Override
+    public void printButton() {
+        System.out.println("Ios button...");
+
+    }
+
+}
