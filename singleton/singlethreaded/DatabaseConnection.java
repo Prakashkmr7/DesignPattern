286398535bdad68216d06b4a5d2bb404fbed8ede
Jai Prakash Kumar <37208098+Prakashkmr7@users.noreply.github.com>
2024-08-29 00:15:46 +0530

    Update DatabaseConnection.java

--- singleton/singlethreaded/DatabaseConnection.java
+++ singleton/singlethreaded/DatabaseConnection.java
@@ -11,11 +11,13 @@ public class DatabaseConnection {
     //If we will create private constructor we need to provide method getInstance method of
     //type static to create object if we will not create then this class is of no use.
 
-    public static DatabaseConnection getInstance() {
-
-		if(instance==null)
+    public static DatabaseConnection getInstance() {//making this method static as we don't have object of this method, so to call without 
+	    //object.And this getInstance() method will provide object of class.
+/**Inorder to create only 1 object and return the same objcet if already exists keeping this if condition.
+	    **/
+		if(instance==null)//Also static method cannot access non-static attribute so make it static in declaration.
 		{
-			instance = new DatabaseConnection();//creating instance else return instance
+			instance = new DatabaseConnection();//creating instance else return instance(this statement will execute only one time)
 		}
         return instance;
     }
